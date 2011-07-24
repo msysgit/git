f22bdab7caee19f55fecbfdd4b44ef8c66b2d5dc
Sverre Rabbelier <srabbelier@gmail.com>
2011-07-24 15:54:04 +0200

    t9350: point out that refs are not updated correctly
    This happens only when the corresponding commits are not exported in
    the current fast-export run. This can happen either when the relevant
    commit is already marked, or when the commit is explicitly marked
    as UNINTERESTING with a negative ref by another argument.
    
    This breaks fast-export basec remote helpers.
    
    Signed-off-by: Sverre Rabbelier <srabbelier@gmail.com>

--- t/t9350-fast-export.sh
+++ t/t9350-fast-export.sh
@@ -496,6 +496,16 @@ test_expect_success 'refs are updated even if no commits need to be exported' '
 		--export-marks=tmp-marks master > /dev/null &&
 	git fast-export --import-marks=tmp-marks \
 		--export-marks=tmp-marks master > actual &&
+'
+
+cat > expected << EOF
+reset refs/heads/master
+from $(git rev-parse master)
+
+EOF
+
+test_expect_failure 'refs are updated even if no commits need to be exported' '
+	git fast-export master..master > actual &&
 	test_cmp expected actual
 '
 
